59b35c4e9b5c55c4f8719d50c3d78fc7388ad2a1
Aviv Navon <aviv@altimo.io>
2021-01-29 11:03:06 +0200

    fix

--- inclearn/lib/data/datasets.py
+++ inclearn/lib/data/datasets.py
@@ -310,19 +310,19 @@ class AwA2(DataHandler):
 
 
 class OurCUB200(DataHandler):
-    train_transforms = transforms.Compose([
+    train_transforms = [
         transforms.Resize((256, 256)),
         transforms.RandomCrop(224),
         transforms.RandomHorizontalFlip(),
         # transforms.ToTensor(),
         # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-    ])
+    ]
 
-    test_transforms = transforms.Compose([
+    test_transforms = [
         transforms.Resize((224, 224)),
         # transforms.ToTensor(),
         # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-    ])
+    ]
 
 
     # train_transforms = [
